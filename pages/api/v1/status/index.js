eb9d0d66d5bfe01739b4b4b51ad5dcfe698fe894
Vitor Gabriel de Souza <72284310+VitorTo@users.noreply.github.com>
2026-02-08 19:45:14 +0000

    finalize endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,11 +3,25 @@ import database from "infra/database";
 async function status(req, res) {
   const updateAt = new Date().toISOString();
 
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
+  const version = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query(
+    "SELECT count(1) FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+
+  const versionValue = version.rows[0].server_version;
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+  const openedConnectionsValue = openedConnections.rows[0].count;
 
   return res.status(200).json({
-    update_at: updateAt,
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: versionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: parseInt(openedConnectionsValue),
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET STATUS ENDPOINT", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
-  expect(res.status).toBe(200);
+  const resBody = await res.json();
+
+  expect(resBody.updated_at).toBeDefined();
+
+  const parseUpdateAt = new Date(resBody.updated_at).toISOString();
+  expect(resBody.updated_at).toEqual(parseUpdateAt);
+
+  const { version, max_connections, opened_connections } =
+    resBody.dependencies.database;
+
+  expect(version).toEqual("16.0");
+  expect(max_connections).toEqual(100);
+  expect(opened_connections).toEqual(1);
 });
