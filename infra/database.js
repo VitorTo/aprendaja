a92a27d6209bfd0b3fddedc761cd53ff0b2bab99
vitorSouza <vitorg.souza08@gmail.com>
2026-02-15 09:34:01 -0300

    add env PORT in Cliente

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ export default {
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: 5432,
+    port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
